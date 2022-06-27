6acb9ff2fb66170cb5f354d902aac88ad9171a97
MKTareen07 <108220352+MKTareen07@users.noreply.github.com>
2022-06-27 15:19:16 +0430

    Add files via upload

---
+++ Converting_C_to_F/Converting_C_to_F.py
@@ -0,0 +1,5 @@
+value= input("Please enter the Celsius" + "\n")
+tempValue= float(value)
+fahrenheit = (tempValue * 1.8) + 32
+newTempValue= str(fahrenheit)
+print("Celsius of "+ value + " = "+ newTempValue)
