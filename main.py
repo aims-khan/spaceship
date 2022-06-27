a99fc41a2da178dd30e9c31121e7aead7a109752
MKTareen07 <108220352+MKTareen07@users.noreply.github.com>
2022-06-27 13:58:20 +0430

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 import pygame
 import os
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT= 900, 500
 
-WIN  = pygame.display.set_mode((WIDTH,HEIGHT))
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Spaceship evening")
-WHITE = (255,255,255)
-SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_yellow.png"))
-YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+WHITE=(255,255,255)
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT= 55, 40
 
-RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join("assets", "spaceship_red.png"))
-RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
+YELLOW_SPACESHIP_IMAGE= pygame.image.load(os.path.join("Assets", "spaceship_yellow.png"))
+YELLOW_SPACESHIP= pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP_IMAGE= pygame.image.load(os.path.join("Assets", "spaceship_red.png"))
+RED_SPACESHIP= pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 def draw_window():
-    WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP,(300,100))
-    WIN.blit(RED_SPACESHIP,(700,100))
-    pygame.display.update()
-    
+	WIN.fill(WHITE)
+	WIN.blit(YELLOW_SPACESHIP,(300,100))
+	WIN.blit(YELLOW_SPACESHIP,(700,100))
+	pygame.display.update()
+
 def main():
-    run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type ==  pygame.QUIT:
-                run = False
-            draw_window()
-    pygame.quit()
-        
+	run= True
+	while run:
+		for event in pygame.event.get():
+			if event.type== pygame.QUIT:
+				run= False
+		draw_window()
+	pygame.quit()
+
 if __name__ == "__main__":
-    main()
-             
+	main()
+
